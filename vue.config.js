c10d28bf8c35ac38ec70bce17c67fc42231c0a9b
VUIVR <asostrovskiy@yandex.ru>
2022-11-01 20:35:00 +0400

    11

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
 /* module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/composition/' : '/'
 } */
-module.exports = {
+/* module.exports = {
   publicPath: 'composition/'
 }
+ */
